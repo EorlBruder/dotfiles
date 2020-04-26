e506bdaa736156853b95ba343a24f2fb97487c29
EorlBruder <eorl@bruder.space>
2020-04-26 18:59:49 +0200

    added xsane alias

--- graphical/.config/zsh/local/alias-graphical.zsh
+++ graphical/.config/zsh/local/alias-graphical.zsh
@@ -1,3 +1,4 @@
 export QT_GRAPHICSSYSTEM=gtk
 alias startx='ssh-agent startx'
+alias xsane-samsung='xsane "smfp:net;192.168.3.2"'
 alias scrot-window="scrot '%Y-%m-%d-%R_$wx$h_scrot.png' -s -e 'mv $f ~/sync/mess/current'"
