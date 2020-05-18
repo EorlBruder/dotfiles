9e463909b2c7e1c12a6a0d4db65d5c73eeb8ac5b
EorlBruder <eorl@bruder.space>
2020-05-18 23:38:20 +0200

    added new aliases

--- graphical/.config/zsh/local/alias-graphical.zsh
+++ graphical/.config/zsh/local/alias-graphical.zsh
@@ -2,3 +2,8 @@ export QT_GRAPHICSSYSTEM=gtk
 alias startx='ssh-agent startx'
 alias xsane-samsung='xsane "smfp:net;printer.rohan"'
 alias scrot-window="scrot '%Y-%m-%d-%R_$wx$h_scrot.png' -s -e 'mv $f ~/sync/mess/current'"
+alias open="xdg-open"
+alias preview-pipe="pandoc > /tmp/preview.html; open /tmp/preview.html"
+preview() {
+  cat $1 | preview-pipe
+}
