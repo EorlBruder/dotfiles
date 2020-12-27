5c3a5a81befa7922f439c1afff18b43b6e67541a
EorlBruder <eorl@bruder.space>
2020-12-27 21:10:01 +0100

    added fancy boxes

--- prod/.config/md2pdf/fences-filter.lua
+++ prod/.config/md2pdf/fences-filter.lua
@@ -1,13 +1,17 @@
 -- convert all fences into latex-environments ending in "Box". 
 function Div(el)
-  elClass = el.classes[1]
+  fenceName = el.classes[1]
+  title = el.attributes["title"]
+  if title == nil then
+    title = ""
+  end
   -- insert element in front
   table.insert(
     el.content, 1,
-    pandoc.RawBlock("latex", "\\begin{" .. elClass .. "Box}"))
+    pandoc.RawBlock("latex", "\\begin{" .. fenceName .. "Box}{" .. title .. "}"))
   -- insert element at the back
   table.insert(
     el.content,
-    pandoc.RawBlock("latex", "\\end{" .. elClass .. "Box}"))
+    pandoc.RawBlock("latex", "\\end{" .. fenceName .. "Box}"))
   return el
 end
