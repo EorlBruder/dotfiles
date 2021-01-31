b1ed9b3bd43c8c21dee24313f4b0ab299b2644d9
EorlBruder <eorl@bruder.space>
2021-01-31 17:46:49 +0100

    fixed changes breaking on full blocks

--- prod/.config/md2pdf/criticmarkup.lua
+++ prod/.config/md2pdf/criticmarkup.lua
@@ -1,23 +1,38 @@
 -- Enable Criticmarkup
 function RawInline(el)
-  return processBlockOrInline(el)
+  return processBlockOrInline(el, false)
 end
 
 function RawBlock(el)
-  return processBlockOrInline(el)
+  return processBlockOrInline(el, true)
 end
 
-function processBlockOrInline(el)
-  print(el.text)
+function processBlockOrInline(el, block)
   -- Replace HTML-Comments with latex-comments
-  if (el.format == "html") and (string.starts(el.text, "<!--")) then
-    return pandoc.RawInline("tex", "\\chcomment{" .. string.gsub(string.gsub(el.text, "<!%-%-% ", ""), "%-%->", "") .. "}")
+  if is_html(el.format) and (string.starts(el.text, "<!--")) then
+    newText = "\\chcomment{" .. string.gsub(string.gsub(el.text, "<!%-%-% ", ""), "%-%->", "") .. "}" 
+    if block then
+      return pandoc.RawBlock("tex", newText)
+    end
+    return pandoc.RawInline("tex", newText)
   end
   -- Replace preprocessed changes-tags with prefixed changes-tags
-  if (el.format == "tex") and (string.starts(el.text,"\\added") or string.starts(el.text,"\\deleted") or string.starts(el.text, "\\replaced") or string.starts(el.text, "\\highlight") or string.starts(el.text, "\\comment")) then
-    return pandoc.RawInline("tex", string.gsub(el.text, "\\", "\\ch"))
+  if is_tex(el.format) and (string.starts(el.text,"\\added") or string.starts(el.text,"\\deleted") or string.starts(el.text, "\\replaced") or string.starts(el.text, "\\highlight") or string.starts(el.text, "\\comment")) then
+    newText = string.gsub(el.text, "\\", "\\ch")
+    if block then
+      return pandoc.RawBlock("tex", newText)
+    end
+    return pandoc.RawInline("tex", newText)
   end
   return el
+end
+
+function is_tex(format)
+    return format == 'latex' or format == 'tex' or format == 'context'
+end
+
+function is_html (format)
+    return format == 'html' or format == 'html4' or format == 'html5'
 end
 
 function string.starts(fullString,substring)
