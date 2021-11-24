de53ac319f8fbc6174640cc140cf2b2f8b1a0931
EorlBruder <eorl@bruder.space>
2021-11-24 15:11:56 +0100

    improved graphical startup

--- graphical/.config/zsh/local/alias-graphical.zsh
+++ graphical/.config/zsh/local/alias-graphical.zsh
@@ -1,5 +1,4 @@
 export QT_GRAPHICSSYSTEM=gtk
-alias startx='ssh-agent startx'
 alias xsane-samsung='xsane "smfp:net;printer.rohan"'
 alias scrot-window="scrot '%Y-%m-%d-%R_$wx$h_scrot.png' -s -e 'mv $f ~/sync/mess/current'"
 alias open="xdg-open"
