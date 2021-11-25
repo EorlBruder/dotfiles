1358319efbd0c5fa91b249e25321fe8c8f3f015e
EorlBruder <eorl@bruder.space>
2021-11-25 16:05:50 +0100

    battery check now gets correct DBUS-SESSION

--- graphical/.bin/battery-warning.sh
+++ graphical/.bin/battery-warning.sh
@@ -2,5 +2,8 @@
 
 BATTINFO=`acpi -b`
 if [[ `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < 00:15:00 ]] ; then
-    DISPLAY=:0.0 /usr/bin/notify-send "low battery" "$BATTINFO"
+  username=$(/usr/bin/whoami)
+  pid=$(pgrep -u $username dunst)
+  dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$pid/environ | sed 's/DBUS_SESSION_BUS_ADDRESS=//' )
+  DBUS_SESSION_BUS_ADDRESS=$dbus /usr/bin/notify-send "low battery" "$BATTINFO"
 fi
