eb37a4c98843b288efc0ce5f6a9192dbd4cf65a3
EorlBruder <eorl@bruder.space>
2021-11-25 01:19:40 +0100

    No notifications shown on lock-screen and kill keepmenu on lock

--- graphical/.bin/i3-lock.sh
+++ graphical/.bin/i3-lock.sh
@@ -1,3 +1,10 @@
 if ! pidof i3lock; then
-  i3lock --ignore-empty-password --clock --date-str="%d-%m-%Y" --time-str="%H:%M" --keylayout 0
+  # Kill keepmenu-daemon
+  pgrep -a python | grep keepmenu | awk '{print $1}' | xargs kill
+  # Pause dunst, so no notifications get shown
+  pkill -u "$USER" -USR1 dunst
+  # Lock the screen. Not forking, for dunst-suspending to work
+  i3lock --ignore-empty-password --clock --date-str="%d-%m-%Y" --time-str="%H:%M" --keylayout 0 -n
+  # continue dunst
+  pkill -u "$USER" -USR2 dunst
 fi
